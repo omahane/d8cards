e7ea410ebd5e1ecabf29997954ff8d4c5bd14766
Christian Burk <christian@skyriter.com>
2022-09-14 09:08:02 -0500

    Adds comments from Sipos' book

--- d8cards03_config_forms/src/Form/SalutationConfigurationForm.php
+++ d8cards03_config_forms/src/Form/SalutationConfigurationForm.php
@@ -5,12 +5,13 @@ use Drupal\Core\Form\ConfigFormBase;
 use Drupal\Core\Form\FormStateInterface;
 
 /**
- * Confifuration form definition for the salutation message.
+ * Configuration form definition for the salutation message.
  */
 class SalutationConfigurationForm extends ConfigFormBase {
     /**
      * {@inheritdoc}
      */
+    // This function is returning an array of the config names we are editing.
     protected function getEditableConfigNames()
     {
         return ['d8cards03_config_forms.custom_salutation'];
@@ -26,22 +27,30 @@ class SalutationConfigurationForm extends ConfigFormBase {
      * {@inheritdoc}
      */
     public function buildForm(array $form, FormStateInterface $form_state) {
+        // This assignment loads our editable configuration object from the Drupal configuration factory
+        // and check the value currently stored in it.
         $config = $this->config('d8cards03_config_forms.custom_salutation');
+        // Defining our form elements.
         $form['salutation'] = array(
             '#type' => 'textfield',
             '#title' => $this->t('Salutation'),
             '#description' => $this->t('Please provide the salutation you want to use.'),
+            // The value present when the user goes to the form.
             '#default_value' => $config->get('salutation'),
         );
+        // Provides the form submit button.
         return parent::buildForm($form, $form_state);
     }
     /**
      * {@inheritdoc}
      */
+    // This is the submit handler.
     public function submitForm(array &$form, FormStateInterface $form_state) {
+        // Loads the config to set and save it.
         $this->config('d8cards03_config_forms.custom_salutation')
         ->set('salutation', $form_state->getValue('salutation'))
         ->save();
+        // After submitting, sends a confirmation message to the user
         parent::submitForm($form, $form_state);
     }
 }
